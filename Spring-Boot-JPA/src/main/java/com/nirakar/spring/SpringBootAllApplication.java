89a721f1fb0aae7a3082c266e97a11abe106e82c
Nirakar <nirakarmohanty@gmail.com>
2021-01-16 12:41:23 +0530

    Spring Boot JPA project set up

---
+++ Spring-Boot-JPA/src/main/java/com/nirakar/spring/SpringBootAllApplication.java
@@ -0,0 +1,13 @@
+package com.nirakar.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAllApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAllApplication.class, args);
+	}
+
+}
---
+++ Spring-Boot-JPA/src/test/java/com/nirakar/spring/SpringBootAllApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nirakar.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAllApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
